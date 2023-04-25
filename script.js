059c37c0f8127ac2ec1ad1fcdd3e46ada496468d
Dng16 <dongyang0016@gmail.com>
2023-04-25 18:54:55 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const menu = document.querySelector('.menu');
+const openMenu = document.querySelector('.open-menu');
+const closeMenu = document.querySelector('.close-menu');
+const box = document.querySelector('.box');
+const overlay = document.querySelector('.overlay');
+const dropdown = document.querySelectorAll('.dropdown');
+
+
+const toggle = () =>{
+  openMenu.classList.toggle('hidden');
+  closeMenu.classList.toggle('hidden');
+  overlay.classList.toggle('show');
+  box.classList.toggle('show');
+
+}
+
+
+
+for(const item of dropdown){
+  item.addEventListener('click',(e) =>{
+    item.classList.toggle('show-links');
+  
+  });
+}
+
+menu.addEventListener('click',toggle);
+overlay.addEventListener('click',toggle);
