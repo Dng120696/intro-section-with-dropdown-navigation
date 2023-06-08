2bf792bd12ba9c83e63d8df23b19c3aaa571a272
Dng16 <dongyang0016@gmail.com>
2023-06-08 10:17:46 +0800

    final commit

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
+const menu = document.querySelector(".menu");
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const box = document.querySelector(".box");
+const overlay = document.querySelector(".overlay");
+const dropdown = document.querySelectorAll(".dropdown");
 
-const menu = document.querySelector('.menu');
-const openMenu = document.querySelector('.open-menu');
-const closeMenu = document.querySelector('.close-menu');
-const box = document.querySelector('.box');
-const overlay = document.querySelector('.overlay');
-const dropdown = document.querySelectorAll('.dropdown');
+const toggle = () => {
+  openMenu.classList.toggle("hidden");
+  closeMenu.classList.toggle("hidden");
+  overlay.classList.toggle("show");
+  box.classList.toggle("show");
+};
 
-
-const toggle = () =>{
-  openMenu.classList.toggle('hidden');
-  closeMenu.classList.toggle('hidden');
-  overlay.classList.toggle('show');
-  box.classList.toggle('show');
-
-}
-
-
-
-for(const item of dropdown){
-  item.addEventListener('click',() =>{
-    item.classList.toggle('show-links');
-  
+dropdown.forEach((drop) => {
+  drop.addEventListener("click", () => {
+    drop.classList.toggle("show-links");
+    dropdown.forEach((otherdrop) => {
+      if (otherdrop !== drop) {
+        otherdrop.classList.remove("show-links");
+      }
+    });
   });
-}
+});
 
-menu.addEventListener('click',toggle);
-overlay.addEventListener('click',toggle);
+menu.addEventListener("click", toggle);
+overlay.addEventListener("click", toggle);
